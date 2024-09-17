17657dca08b2baee391c576d8fdd5d830828f673
Gustavo <gstvrrds@gmail.com>
2024-09-17 20:34:03 -0300

    feat:adicionando injetaveis

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,9 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Injectable, Module } from "@nestjs/common";
 import { UsuarioController } from "./usuario.controller";
+import { UsuarioRepository } from "./usuario.repository";
+
 
 @Module({
     imports:[],
     controllers:[UsuarioController],
-    providers:[]
+    providers:[UsuarioRepository]
 })
 export class UsuarioModule{};
