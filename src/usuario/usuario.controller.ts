72aa723d710ca9f8df39669f8f3900043f558d3a
Gustavo <gstvrrds@gmail.com>
2024-10-14 17:34:27 -0300

    feat:criando dto de listagem

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,33 +1,43 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { CriaUsuarioDTO } from "./dto/CriaUsuario.dto";
-import { UsuarioEntity } from "./usuario.entity";
-import {v4 as vuuid} from 'uuid'
+import { UsuarioEntity } from './usuario.entity';
+import { v4 as vuuid } from 'uuid';
+import { ListaUsuarioDto } from "./dto/ListaUsuario.dto";
 
 //Dentro do controler coloca a rota
 @Controller('/usuarios')
-export class UsuarioController{
+export class UsuarioController {
+  constructor(private usuarioRepository: UsuarioRepository) {}
 
+  //Tipo da requisição
+  @Post()
+  //Rota
+  async criaUsuario(@Body() dadosDoUsuario:CriaUsuarioDTO ){
+    const usuarioEntity = new UsuarioEntity();
+     usuarioEntity.email = dadosDoUsuario.email;
+    usuarioEntity.nome = dadosDoUsuario.nome;
+    usuarioEntity.senha = dadosDoUsuario.senha;
+    usuarioEntity.id = vuuid();
+    this.usuarioRepository.salvar(usuarioEntity);
     
-    constructor(private usuarioRepository:UsuarioRepository){}
-
-    //Tipo da requisição
-    @Post()
-    //Rota
-    async criaUsuario(@Body() dadosDoUsuario:CriaUsuarioDTO){
-        const usuarioEntity = new UsuarioEntity()
-        usuarioEntity.email = dadosDoUsuario.email;
-        usuarioEntity.nome = dadosDoUsuario.nome;
-        usuarioEntity.senha = dadosDoUsuario.senha;
-        usuarioEntity.id = vuuid();
-        this.usuarioRepository.salvar(usuarioEntity)
-        return {id:usuarioEntity.id,
-                message:"Usuario criado com sucesso!"};
+    return {usuario: new ListaUsuarioDto(
+      usuarioEntity.id,
+      usuarioEntity.nome
+    ),
+              message:"Usuario criado com sucesso!"};
     }
 
     @Get()
     async pegaUsers(){
-        return this.usuarioRepository.getUsers();
-    }
+        const usuariosSalvos = await  this.usuarioRepository.getUsers();
+        const usuariosLista = usuariosSalvos.map(
+          usuario => new ListaUsuarioDto(
+            usuario.id,
+            usuario.nome
+            
+        ));
+        return usuariosLista;
+      }
 
 }
